d26e3dc389daf5cc6be0027f31547e0d39224394
Frans Kuipers <franskuipers@gmail.com>
2018-02-06 22:04:21 +0100

    version 2.0 for drupal7 installs

---
+++ drush/aliases/aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+
+$aliases['default'] = array(
+  'root' => '/var/www/docroot',
+  'uri'  => 'drupal7-advanced.docksal',
+);
+
+#$aliases['anothersite'] = array(
+#  'root' => '/var/www/docroot',
+#  'uri'  => 'anothersite.drupal7-advanced.docksal',
+#);
